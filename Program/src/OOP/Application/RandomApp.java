9b4860b2619ee89a229a51679a58a274ff29d99a
Abdullah Farauk <abdullahfarauk@gmail.com>
2022-04-09 00:34:28 +0700

    commit from intellij

---
+++ Program/src/OOP/Application/RandomApp.java
@@ -0,0 +1,7 @@
+package OOP.Application;
+
+public class RandomApp {
+    public static void main(String[] args) {
+        System.out.println("Ini adalah file agar bisa commit melalui intellij idea.");
+    }
+}
