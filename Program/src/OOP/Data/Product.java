a6328dd1eb1450f0ed55f865667e69292102c782
Abdullah Farauk <abdullahfarauk@gmail.com>
2022-04-09 00:29:15 +0700

    commit from intellij

--- Program/src/OOP/Data/Product.java
+++ Program/src/OOP/Data/Product.java
@@ -1,8 +1,8 @@
 package OOP.Data;
 
 public class Product {
-    protected String name;
-    protected int price;
+    public String name;
+    public int price;
 
     Product(String name, int price){
         this.name = name;
@@ -25,4 +25,44 @@ public class Product {
     public String toString(){
         return "Product name: " + getName() + ", price: " + getPrice();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (price != product.price) return false;
+        return name != null ? name.equals(product.name) : product.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + price;
+        return result;
+    }
+
+    /*public boolean equals(Object o){
+        if(o == this){
+            return true;
+        }
+
+        if(!(o instanceof Product)){
+            return false;
+        }
+
+        Product product = (Product) o;
+
+        if(this.price != product.price){
+            return false;
+        }
+
+        if(this.name != null){
+            return this.name.equals(product.name);
+        }else {
+            return product.name == null;
+        }
+    }*/
 }
